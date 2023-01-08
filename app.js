0d4d9ef0db7fb4df589012c5340fe4922cc18602
Mary8992 <mc046569@gmail.com>
2023-01-08 18:41:39 +0300

    app.js created

---
+++ app.js
@@ -0,0 +1,14 @@
+function validation () {
+    let form = document.getElementById("form");
+    let email = document.getElementById("email").value;
+    let text = document.getElementById("text");
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern)) {
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+    } else {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+    }
+}

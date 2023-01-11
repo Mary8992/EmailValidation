a12e50816e3c4cc533b7f8656748f928014284fc
Mary8992 <mc046569@gmail.com>
2023-01-11 12:38:01 +0300

    colorful heart feature added

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+document.addEventListener('mousemove', function(e) {
+    let body = document.querySelector('body');
+    let heart = document.createElement('span');
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    heart.style.left = x + 'px';
+    heart.style.top = y + 'px';
+
+    let size = Math.random() * 100;
+    heart.style.width = 20 + size + 'px';
+    heart.style.height = 20 + size + 'px';
+
+    body.appendChild(heart);
+
+    setTimeout(function() {
+        heart.remove();
+    }, 2000)
+})
+
+
+
+//Email Validation
 function validation () {
     let form = document.getElementById("form");
     let email = document.getElementById("email").value;
